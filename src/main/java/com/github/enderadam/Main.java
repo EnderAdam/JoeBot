313b5484559450a8f9cb215f218cac007baeea71
EnderAdam <t.hasankurnaz08@gmail.com>
2021-11-04 13:08:28 +1300

    Added isKicking to monkey emoji

--- src/main/java/com/github/enderadam/Main.java
+++ src/main/java/com/github/enderadam/Main.java
@@ -169,8 +169,10 @@ public class Main {
 //                }
                 if (eventReaction.getEmoji().equalsEmoji("🐒")) {
 //                    eventReaction.getUser().get().sendMessage("SPAM");
-                    if (!eventReaction.getUserIdAsString().equalsIgnoreCase("246637425961467904")) {
-                        message.getServer().get().kickUser(api.getUserById(eventReaction.getUserIdAsString()).join());
+                    if (isKicking) {
+                        if (!eventReaction.getUserIdAsString().equalsIgnoreCase("246637425961467904")) {
+                            message.getServer().get().kickUser(api.getUserById(eventReaction.getUserIdAsString()).join());
+                        }
                     }
 
                 }
